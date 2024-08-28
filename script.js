312ab3be96e8e103d3d8097b754bdc33c9afc27d
Heverton Pinheiro <hevertonpinheiro@yahoo.com.br>
2024-08-27 21:12:21 -0300

    html and css finished

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    html.classList.toggle('light')
+    
+    // //OU....
+    // if(html.classList.contains('light')) {
+    //     html.classList.remove('light')
+    // } else {
+    //     html.classList.add('light')
+    // }
+
+}
